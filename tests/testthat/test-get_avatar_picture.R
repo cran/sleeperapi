d2708a2d35aceaadbcd4e5ca46fadf4900d9a29c
Nick Bultman <njbultman74@gmail.com>
2024-05-15 02:40:03 +0000

    version 1.1.0

--- tests/testthat/test-get_avatar_picture.R
+++ tests/testthat/test-get_avatar_picture.R
@@ -2,5 +2,6 @@
 
 # Test 1: Error thrown if invalid type is specified
 test_that("Returns error if invalid type specified", {
-  expect_error(get_avatar_picture("c751b27d9158c1dd41bd33dc7e4bcdde", type = "invalid"), "Invalid value entered for type: can only be 'full' or 'thumbnail'")
+  expect_error(get_avatar_picture("c751b27d9158c1dd41bd33dc7e4bcdde", type = "invalid", keep_image = FALSE), "Invalid value entered for type: can only be 'full' or 'thumbnail'")
 })
+
